e8a91595253381206a4e12c5e629c56f5afe13b6
stevebosworth <steve.bosworth@shopify.com>
2015-02-07 23:18:03 -0500

    some stuff

--- client/test-ajax.js
+++ client/test-ajax.js
@@ -1,26 +1,33 @@
 var http = require('http');
+var j5 = require('johnny-five');
 
-var options = {
-  host: 'localhost',
-  port: '3000',
-  path: '/table',
-  method: 'POST',
-  headers: {
-        "Content-Type": "application/json"
-    }
-};
 
-function callback (res) {
-  console.log('STATUS: ' + res.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(res.headers));
-  res.setEncoding('utf8');
-  res.on('data', function (chunk) {
-    console.log('BODY: ' + chunk);
-  });
+
+
+function update() {
+  var options = {
+    host: 'localhost',
+    port: '3000',
+    path: '/table',
+    method: 'POST',
+    headers: {
+          "Content-Type": "application/json"
+      }
+  };
+
+  function callback (res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+    });
+  }
+
+  var req = http.request(options, callback);
+  // write the request parameters
+  req.write('{"key": "hello", "status": true}');
+  req.end();
 }
 
-var req = http.request(options, callback);
-// write the request parameters
-req.write('{"key": "hello", "status": true}');
-req.end();
 
